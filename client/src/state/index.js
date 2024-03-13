0cc7e2961087dc3bc1df05c32156712f2311c331
nikoavgerinos <avgerinos.niko@gmail.com>
2024-03-13 13:15:49 +0100

    feat: inital setup navbar & setup redux state via redux toolkit

---
+++ client/src/state/index.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    isCartOpen: false,
+    cart: [],
+    items: [],
+}
+
+export const cartSlice = createSlice({
+    name: "cart",
+    initialState,
+    reducers: {
+        setItems: (state, action) => {
+            state.items = action.payload;
+        }
+    }
+});
+
+export const {
+    setItems
+} = cartSlice.actions;
+
+export default cartSlice.reducer;
