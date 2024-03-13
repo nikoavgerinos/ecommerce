c0f4dd905c31c06e16539deaac58f925e2ea2543
nikoavgerinos <avgerinos.niko@gmail.com>
2024-03-13 12:45:06 +0100

    feat: setup component routing in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,11 @@ import {
   BrowserRouter, Routes, Route, useLocation
 } from 'react-router-dom';
 import Home from "./scenes/home/Home";
+import ItemDetails from "./scenes/itemDetails/ItemDetails";
+import Checkout from "./scenes/checkout/Checkout";
+import Confirmation from "./scenes/checkout/Confirmation";
+
+
 const ScrollToTop = () => {
   const { pathname } = useLocation();
 
@@ -21,6 +26,9 @@ function App() {
         <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="item/:itemId" element={<ItemDetails />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="checkout/success" element={<Confirmation />} />
         </Routes>
       </BrowserRouter>
 
